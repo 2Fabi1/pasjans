186ac8e2a2378001f72c8d9ddd417ef9952f158d
2Fabi1 <158741663+2Fabi1@users.noreply.github.com>
2025-05-29 16:51:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,6 @@ def ruch(karty_przenoszone, droga, kolumny, numer_kolumny=0, niezakryte=0, cel_k
                     print(f"Nie można przenieść {karty_przenoszone[0]} na {kolumny[cel_kolumna-1][-1]}.")
                     return kolumny
                 system("cls")
-                print(f"Przeniesiono {', '.join(str(karta) for karta in karty_przenoszone)} do kolumny {cel_kolumna}.")
-
             if niezakryte == len(karty_przenoszone) and len(kolumny[numer_kolumny-1]) > 0:
                 kolumny[numer_kolumny-1][-1].zakryta = False
     elif droga == "kol-koń":
@@ -57,10 +55,6 @@ def ruch(karty_przenoszone, droga, kolumny, numer_kolumny=0, niezakryte=0, cel_k
             if niezakryte == 1 and len(kolumny[numer_kolumny-1]) > 0:
                 kolumny[numer_kolumny-1][-1].zakryta = False
             system("cls")
-            if karty_przenoszone.kolor in ["♥", "♦"]:
-                print(f"Przeniesiono kartę {karty_przenoszone} do stosu końcowego \033[91m{karty_przenoszone.kolor}\033[00m.")
-            else:
-                print(f"Przeniesiono kartę {karty_przenoszone} do stosu końcowego \033[96m{karty_przenoszone.kolor}\033[00m.")
         else:
             system("cls")
             print(f"Nie można przenieść {karty_przenoszone} do tej kolumny.")
@@ -71,7 +65,6 @@ def ruch(karty_przenoszone, droga, kolumny, numer_kolumny=0, niezakryte=0, cel_k
                 stos_r.remove(karty_przenoszone)
                 obecna_karta_idx -= 1
                 system("cls")
-                print(f"Przeniesiono kartę {karty_przenoszone} do pustej kolumny {cel_kolumna}.")
             else:
                 system("cls")
                 print("Do pustej kolumny można przenieść tylko króla.")
@@ -80,10 +73,8 @@ def ruch(karty_przenoszone, droga, kolumny, numer_kolumny=0, niezakryte=0, cel_k
             stos_r.remove(karty_przenoszone)
             obecna_karta_idx -= 1
             system("cls")
-            print(f"Przeniesiono kartę {karty_przenoszone} do kolumny {cel_kolumna}.")
         else:
             system("cls")
-            print(f"Nie można przenieść {karty_przenoszone} na {kolumny[cel_kolumna-1][-1]}.")
         return kolumny, obecna_karta_idx
     elif droga == "rez-koń":
         lista_koloru = kolory_listy[kolory.index(karty_przenoszone.kolor)]
@@ -91,25 +82,16 @@ def ruch(karty_przenoszone, droga, kolumny, numer_kolumny=0, niezakryte=0, cel_k
             lista_koloru.append(karty_przenoszone)
             stos_r.remove(karty_przenoszone)
             obecna_karta_idx -= 1
-            system("cls")
-            if karty_przenoszone.kolor in ["♥", "♦"]:
-                print(f"Przeniesiono kartę {karty_przenoszone} do stosu końcowego \033[91m{karty_przenoszone.kolor}\033[00m.")
-            else:
-                print(f"Przeniesiono kartę {karty_przenoszone} do stosu końcowego \033[96m{karty_przenoszone.kolor}\033[00m.")
-        else:
-            system("cls")
-            print(f"Nie można przenieść {karty_przenoszone} do stosu końcowego.")
+        system("cls")
         return kolumny, obecna_karta_idx
     elif droga == "koń-kol":
         if len(kolumny[cel_kolumna-1]) == 0 and karty_przenoszone.wartosc == "K":
             kolumny[cel_kolumna-1].append(karty_przenoszone)
             system("cls")
-            print(f"Przeniesiono kartę {karty_przenoszone} do kolumny {cel_kolumna}.")
         elif weryfikacja_ruchu(karty_przenoszone, kolumny[cel_kolumna-1][-1]):
             kolumny[cel_kolumna-1].append(karty_przenoszone)
             kolory_listy[kolory.index(karty_przenoszone.kolor)].remove(karty_przenoszone)
             system("cls")
-            print(f"Przeniesiono kartę {karty_przenoszone} do kolumny {cel_kolumna}.")
             return kolumny, kolory_listy
         else:
             system("cls")
@@ -150,26 +132,9 @@ def main(kolumny, obecna_karta_idx, kolory_listy, stos_r, talia):
                     kolumna = kolumny[int(wybor[0])-1]
                     if kolumna:
                         niezakryte_count = niezakryte_karty(kolumna)
-                        if niezakryte_count == 1:
-                            karty_przenoszone = []
-                            karty_przenoszone.append(kolumna[-1])
-                            kolumny = ruch(karty_przenoszone, "kol-kol", kolumny, int(wybor[0]), niezakryte_count, int(wybor[2]))  # Przeniesienie ostatniej karty
-                        else:
-                            print("Karty do wyboru:")
-                            j = 0
-                            for i in range(len(kolumna)):
-                                if not(kolumna[i].zakryta):
-                                    print(f"{j+1} - {kolumna[i]}")
-                                    j += 1
-                            while True:
-                                wybor_karty = int(input(f"Wybierz kartę i wszystkie karty po niej, którą/e chcesz przesunąć (1-{niezakryte_count}): "))
-                                if wybor_karty <= niezakryte_count and wybor_karty > 0:
-                                    break
-                                else:
-                                    print("Nieprawidłowy wybór karty.")
-                            karty_przenoszone = kolumna[-1*(niezakryte_count-wybor_karty+1):]
-                            wybor_kolumny = int(wybor[2])
-                            kolumny = ruch(karty_przenoszone, "kol-kol", kolumny, int(wybor[0]), niezakryte_count, wybor_kolumny)
+                        karty_przenoszone = []
+                        karty_przenoszone.append(kolumna[-1])
+                        kolumny = ruch(karty_przenoszone, "kol-kol", kolumny, int(wybor[0]), niezakryte_count, int(wybor[2]))  # Przeniesienie ostatniej karty
                     else:
                         system("cls")
                         print("Kolumna jest pusta.")
@@ -226,10 +191,38 @@ def main(kolumny, obecna_karta_idx, kolory_listy, stos_r, talia):
                     print("Nieprawidłowy wybór kolumny.")
             else:
                 wypisz_bledna_konstrukcje()
+        elif len(wybor) == 5 or len(wybor) == 6: #ochrona przed błędami
+            if wybor[0].isdigit() and wybor[1] == "-" and wybor[2].isdigit() and wybor[3] == "-":   
+                if 1 <= int(wybor[0]) <= 7:
+                    kolumna = kolumny[int(wybor[0])-1]
+                    if kolumna:
+                        niezakryte_count = niezakryte_karty(kolumna)
+                        if wybor[4] == '1':
+                            wartosc_karty = '10'
+                        else:
+                            wartosc_karty = wybor[4]
+                        wybor_karty = 0
+                        for i in range(niezakryte_count):
+                            if kolumna[i+len(kolumna)-niezakryte_count].wartosc == wartosc_karty.upper():
+                                wybor_karty = i+1
+                        if wybor_karty <= niezakryte_count and wybor_karty > 0:
+                            karty_przenoszone = kolumna[-1*(niezakryte_count-wybor_karty+1):]
+                            wybor_kolumny = int(wybor[2])
+                            kolumny = ruch(karty_przenoszone, "kol-kol", kolumny, int(wybor[0]), niezakryte_count, wybor_kolumny)
+                        else:
+                            system("cls")
+                            print("Nieprawidłowy wybór karty.")
+                    else:
+                        system("cls")
+                        print("Kolumna jest pusta.")
+                else:
+                    system("cls")
+                    print("Nieprawidłowy wybór kolumny.")
         else:
             wypisz_bledna_konstrukcje()
         if win(kolory_listy):
-            wyswietl_plansze_gry(kolumny, stos_r, kolory, kolory_listy, obecna_karta_idx)
+            wyswietl_stos_rezerwowy(stos_r, obecna_karta_idx)
+            wyswietl_stos_koncowy(kolory, kolory_listy)
             break
 
 if __name__ == "__main__":
